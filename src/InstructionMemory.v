fb9f61be93c524dd17cb7db6a956199b273478e2
Ishani123456 <106423821+Ishani123456@users.noreply.github.com>
2026-06-30 17:01:14 +0530

    Update InstructionMemory.v

--- src/InstructionMemory.v
+++ src/InstructionMemory.v
@@ -4,21 +4,11 @@ module InstructionMemory(
 );
     reg [31:0] imem [0:255];
 
-  initial begin
-    imem[0] = 32'h00500093;  
-    imem[1] = 32'h00300113;  
-    imem[2] = 32'h002081b3;  
-    imem[3] = 32'h00208463;  
-    imem[4] = 32'h06300213;  
-    imem[5] = 32'h00302023;  
-    imem[6] = 32'h00002283;  
-    imem[7] = 32'h00518463;  
-    imem[8] = 32'h06f00313;  
-    imem[9] = 32'h0de00393;  
-end
-
-    always @(*) begin
-        instruction = imem[address[31:2]];  
+    initial begin
+        $readmemh("program.mem", imem);
     end
 
+    always @(*) begin
+        instruction = imem[address[31:2]];
+    end
 endmodule

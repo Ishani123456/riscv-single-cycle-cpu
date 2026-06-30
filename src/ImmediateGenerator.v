62821c1a87b5e82432f0480deb3a2a2756530587
Ishani123456 <106423821+Ishani123456@users.noreply.github.com>
2026-06-30 16:59:50 +0530

    Fix immediate output width for case 3'b100

--- src/ImmediateGenerator.v
+++ src/ImmediateGenerator.v
@@ -9,7 +9,7 @@ module ImmediateGenerator(
             3'b001: imm_out = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
             3'b010: imm_out = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
             3'b011: imm_out = {instruction[31:12], 12'b0};
-            3'b100: imm_out = {{11{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+            3'b100: imm_out = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
             default: imm_out = 32'b0;
         endcase
     end
